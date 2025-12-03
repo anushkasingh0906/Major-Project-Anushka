0a57610c4d0e107d9ae2c6a864536ab38143713f
anushkasingh0906 <anushkasingh14906@gmail.com>
2025-12-04 01:59:45 +0530

    Update and rename file to functions.h

---
+++ include/functions.h
@@ -0,0 +1,8 @@
+#ifndef FUNCTIONS_H
+#define FUNCTIONS_H
+
+void displayHangman(int wrongAttempts);
+void displayWord(char word[], int guessed[]);
+int isWordGuessed(int guessed[], int length);
+
+#endif
